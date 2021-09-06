b59066869fc5e2fe1f5a01ce5dad3596a2967cec
ahdai1 <1305377980@qq.com>
2021-09-06 21:08:00 +0800

    DAO基类

---
+++ main/src/student/base/BaseDAO.java
@@ -0,0 +1,48 @@
+package student.base;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import student.DAO;
+import student.dbUtil.dbUtil;
+import student.dao.StuDAO;
+import student.dao.AdminDAO;
+
+/**
+ * 说明：DAO基类
+ */
+public abstract class BaseDAO {
+    protected final dbUtil db = dbUtil.getdbUtil();
+    protected ResultSet rs;
+    private static BaseDAO baseDAO;
+
+    public BaseDAO(){}
+
+    public static BaseDAO getDAO(DAO dao){
+        switch (dao){
+            case AdMinDAO:
+                if(baseDAO == null || baseDAO.getClass() != AdminDAO.class){
+                    baseDAO = AdminDAO.getInstance();
+                }
+                break;
+            case StuDAO:
+                if(baseDAO == null || baseDAO.getClass() != StuDAO.class){
+                    baseDAO = StuDAO.getInstance();
+                }
+                break;
+            default:
+                break;
+        }
+        return baseDAO;
+    }
+
+    protected void destroy(){
+        try{
+            if(rs != null){
+                rs.close();
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
