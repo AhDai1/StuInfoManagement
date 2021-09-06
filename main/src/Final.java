2fc2592374a2783de8fb53e8444c5837ebfd8907
ahdai1 <1305377980@qq.com>
2021-09-06 17:11:30 +0800

    一些常量

---
+++ main/src/Final.java
@@ -0,0 +1,58 @@
+/**
+ *说明：定义一些常量
+ */
+public class Final {
+    //jdbc
+    public static final String JDBC_URL = "jdbc:sqlserver://127.0.0.1:1433";
+    public static final String JDBC_DRIVER = "com.mircosoft.sqlserver.jdbc.SQLServerDriver";
+    public static final String JDBC_USERNAME = "sa";
+    public static final String JDBC_PASSWORD = "182715wj";
+
+
+    //student
+    public static final String STUDENT_NAME = "姓名";
+    public static final String STUDENT_SNO = "学号";
+    public static final String STUDENT_SEX = "性别";
+    public static final String STUDENT_FACULTU = "院系";
+    public static final String STUDENT_HOMETOWN = "籍贯";
+    public static final String STUDENT_EMAIL = "电子邮件";
+    public static final String STUDENT_TEL = "联系方式";
+
+    //login
+    public static final String LOGIN_TITLE = "登录界面";
+    public static final String LOGIN_USERNAME = "用户名";
+    public static final String LOGIN_PASSWORD = "密码";
+    public static final String LOGIN = "登录";
+    public static final String RESET = "重置";
+
+    //main
+    public static final String MAIN_TITLE = "学生信息管理系统";
+    public static final String MAIN_DI = "第 ";
+    public static final String MAIN_YE = "/99 页";
+    public static final String MAIN_RESULT = "查询结果";
+    public static final String MAIN_FIRST = "首页";
+    public static final String MAIN_LAST = "末页";
+    public static final String MAIN_PRE = "上一页";
+    public static final String MAIN_NEXT = "下一页";
+    public static final String PARAM_FIND_CONDITION = "";
+    public static final String PARAM_FIND = "查找";
+    public static final String PARAM_ADD = "添加";
+    public static final String PARAM_DELETE = "删除";
+    public static final String PARAM_UPDATE = "更新";
+
+
+    //add
+    public static final String ADD_TITLE = "添加学生信息";
+    public static final String ADD_ADD = "添加";
+    public static final String EXIT = "退出";
+
+    //delete
+    public static final String DELETE_TITLE = "删除学生信息";
+    public static final String DELETE = "删除";
+
+    // update view
+    public static final String UPDATE_TITLE = "更新学生信息";
+    public static final String UPDATE = "更新";
+
+}
+
