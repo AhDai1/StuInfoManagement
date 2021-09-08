f44acbf0e9de217bae7bdb70ea7cb81161867844
ahdai1 <1305377980@qq.com>
2021-09-08 21:04:29 +0800

    修改表

--- main/src/student/run/Main.java
+++ main/src/student/run/Main.java
@@ -18,15 +18,15 @@ public class Main {
                 "name varchar(32)," +
                 "username varchar(32)," +
                 "password varchar(32))");
-        db.execute("insert into admin(id, name, username, password) values(1, 'admin', 'test', 'test')");
+        db.execute("insert into admin(id, name, username, password) values(1, 'admin', '1', '1')");
 
         //student
         db.execute("create table student(" +
-                "id int primary key," +
-                "sno varchar(16)," +
+                "id int identity(1,1) ," +
+                "sno varchar(16) primary key," +
                 "name varchar(32)," +
                 "sex varchar(8)," +
-                "faulty varchar(32)," +
+                "faculty varchar(32)," +
                 "hometown varchar(64)," +
                 "email varchar(32)," +
                 "tel varchar(16))");
