9b162391097893f501d1e6a2ba0ddad8ad88cb2a
ahdai1 <1305377980@qq.com>
2021-09-09 11:25:44 +0800

    增加注册模块

--- main/src/student/dao/AdminDAO.java
+++ main/src/student/dao/AdminDAO.java
@@ -36,4 +36,34 @@ public class AdminDAO extends BaseDAO{
         }
         return result;
     }
+
+    public boolean queryRegister(String name, String username, String password){
+        boolean result =false;
+        boolean flag = false;
+        if(username.length() == 0 || password.length() == 0 || name.length() == 0){
+            return result;
+        }
+        String sql1= "select * from admin where username=? and password=?";
+        String[] param1 ={username, password};
+        rs = db.executeQuery(sql1, param1);
+        try{
+            if(rs.next()){
+                flag= true;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally {
+            destroy();
+        }
+        if(!flag) {
+            String sql2 = "insert into admin(name,username,password) values(?,?,?)";
+            String[] param2 = {name, username, password};
+
+            if (db.executeUpdate(sql2, param2) == 1) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
 }
