935215636cbad17a66ec53538406c2ebde612762
ahdai1 <1305377980@qq.com>
2021-09-11 12:43:34 +0800

    选择界面

---
+++ main/src/student/view/SelectView.java
@@ -0,0 +1,61 @@
+package student.view;
+
+
+import student.Final;
+import student.ScheduleView.MainView;
+import student.run.Main;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+/**
+ * 说明： 选择页面
+ */
+public class SelectView extends JFrame {
+    JPanel jpCenter;
+    JButton jbStuMessage, jbStuScore, jbSchedule;
+
+    public SelectView() { init(); }
+
+    private void init(){
+        this.setTitle(Final.SELECT);
+
+        jpCenter = new JPanel();
+        //jpCenter.setLayout(new GridLayout(3,1, 5,5));
+        //jpCenter.setSize(375,150);
+        jpCenter.setLayout(new FlowLayout(FlowLayout.CENTER, 100,10));
+        jbStuMessage = new JButton(Final.SELECT_MESSAGE);
+        jbStuMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new student.view.MainView();
+            }
+        });
+
+        jbSchedule = new JButton(Final.SELECT_SCHEDULE);
+        jbSchedule.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new student.ScheduleView.MainView();
+            }
+        });
+
+        jbStuScore = new JButton(Final.SELECT_SCORE);
+
+        jpCenter.add(jbStuMessage);
+        jpCenter.add(jbSchedule);
+        jpCenter.add(jbStuScore);
+        //jpCenter.setVisible(true);
+        this.add(jpCenter);
+
+
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setBounds(450, 250, 375, 150);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+}
