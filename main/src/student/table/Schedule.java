ad8df93ea3f337d4f73cd2900a0cb9691451673a
ahdai1 <1305377980@qq.com>
2021-09-11 12:41:49 +0800

    课程表

---
+++ main/src/student/table/Schedule.java
@@ -0,0 +1,24 @@
+package student.table;
+
+/**
+ * 说明：课程表
+ */
+public class Schedule {
+    private String SchNo;
+    private String SchName;
+    private String SchHour;
+    private String SchBook;
+
+    public String getSchNo() { return SchNo; }
+    public void setSchNo(String schno) { this.SchNo = schno; }
+
+    public String getSchName() { return SchName; }
+    public void setSchName(String schname) { this.SchName = schname; }
+
+    public String getSchHour() { return SchHour; }
+    public void setSchHour(String schhour) { this.SchHour = schhour; }
+
+    public String getSchBook() { return SchBook; }
+    public void setSchBook(String schbook) {this.SchBook = schbook; }
+
+}
