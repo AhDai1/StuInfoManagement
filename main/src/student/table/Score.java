d5822056d61151b9310782742892fc9348fa976f
ahdai1 <1305377980@qq.com>
2021-09-12 16:32:15 +0800

    成绩表

---
+++ main/src/student/table/Score.java
@@ -0,0 +1,21 @@
+package student.table;
+
+/**
+ * 说明：成绩表
+ */
+public class Score {
+    private String SchNo;
+    private String StuNo;
+    private int StuScore;
+
+    public String getSchNo() { return SchNo; }
+    public void setSchNo(String schno) { this.SchNo = schno; }
+
+    public String getStuNo() { return StuNo; }
+    public void setStuNo(String stuno) { this.StuNo = stuno; }
+
+    public int getStuScore() { return StuScore; }
+    public void setStuScore(int stuscore) { this.StuScore = stuscore; }
+
+
+}
